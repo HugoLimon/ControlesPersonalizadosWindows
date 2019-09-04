a3198066d1526b9144fca1f19f7d91ce6678939d
Hugo <hugolicas.hl@gmail.com>
2019-09-03 19:59:32 -0500

    Se agrego el textbox con place holder y border radius

---
+++ CustomControls111BTEC/CustomControls111BTEC/111TextBox.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace CustomControls111BTEC
+{
+    partial class _111TextBox
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textHolderLabel = new System.Windows.Forms.Label();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 14);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textHolderLabel
+            // 
+            this.textHolderLabel.AutoSize = true;
+            this.textHolderLabel.BackColor = System.Drawing.Color.Transparent;
+            this.textHolderLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textHolderLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textHolderLabel.Location = new System.Drawing.Point(0, 0);
+            this.textHolderLabel.Name = "textHolderLabel";
+            this.textHolderLabel.Size = new System.Drawing.Size(35, 13);
+            this.textHolderLabel.TabIndex = 1;
+            this.textHolderLabel.Text = "label1";
+            this.textHolderLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuElipse2
+            // 
+            this.bunifuElipse2.ElipseRadius = 5;
+            this.bunifuElipse2.TargetControl = this;
+            // 
+            // _111TextBox
+            // 
+            this.Controls.Add(this.textHolderLabel);
+            this.Controls.Add(this.textBox1);
+            this.Name = "_111TextBox";
+            this.Size = new System.Drawing.Size(121, 14);
+            this.BackColorChanged += new System.EventHandler(this._111TextBox_BackColorChanged);
+            this.ForeColorChanged += new System.EventHandler(this._111TextBox_ForeColorChanged);
+            this.Enter += new System.EventHandler(this._111TextBox_Enter);
+            this.Leave += new System.EventHandler(this._111TextBox_Leave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label textHolderLabel;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
+    }
+}
---
+++ CustomControls111BTEC/CustomControls111BTEC/111TextBox.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomControls111BTEC
+{
+    public partial class _111TextBox : UserControl
+    {
+        private string placeHolderText = "Texto";
+        private bool enablePlaceHolder=true;
+        public _111TextBox()
+        {
+            InitializeComponent();
+            textHolderLabel.Text = placeHolderText;
+            textHolderLabel.BackColor = textBox1.BackColor;
+
+        }
+        #region Propiedades
+        /// <summary>
+        /// Agregas el texto del PlaceHolder
+        /// </summary>
+        [Description("Texto de PlaceHolder"), Category("Style")]
+        public string PalceHolderText
+        {
+            get { return this.placeHolderText; }
+            set
+            {
+                textHolderLabel.Text = value;
+                this.placeHolderText = value;
+      
+            }
+        }
+        /// <summary>
+        /// Bandera para habilitar y deshabilitar el place holder
+        /// </summary>
+        [Description("Habilitar/Deshabilitar PlaceHolder"), Category("Style")]
+        public bool EnablePalceHolder
+        {
+            get { return enablePlaceHolder; }
+            set
+            {
+                if (textHolderLabel.Visible == true)
+                {
+                    textHolderLabel.Visible = value;
+                    enablePlaceHolder = value;
+                }
+                    
+                else
+                {
+                    enablePlaceHolder = value;
+                }
+            }
+        }
+
+        [Description("Habilitar/Deshabilitar PlaceHolder"), Category("Style")]
+        public int BorderRadius
+        {
+            get { return bunifuElipse1.ElipseRadius; }
+            set { bunifuElipse1.ElipseRadius = value; }
+        }
+        /// <summary>
+        /// Color del texto del place holder
+        /// </summary>
+        [Description("PlaceHolder Color"), Category("Style")]
+        public Color PlaceHolderColor
+        {
+            get { return textHolderLabel.ForeColor; }
+            set { textHolderLabel.ForeColor = value; }
+        }
+
+
+        #endregion
+        private void _111TextBox_Leave(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="")
+            {
+                textHolderLabel.Visible = true  && enablePlaceHolder;
+                textHolderLabel.Text = placeHolderText;
+            }
+            
+        }
+
+        private void _111TextBox_Enter(object sender, EventArgs e)
+        {
+            textHolderLabel.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            textHolderLabel.Visible = false;
+            textBox1.Focus();
+            _111TextBox_Enter(sender, e);
+        }
+
+        private void _111TextBox_ForeColorChanged(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = this.ForeColor;
+        }
+
+        private void _111TextBox_BackColorChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
